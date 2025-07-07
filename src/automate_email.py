b9d7db1ba4950980eba66562e33e3416b3ff28bc
Aravind <laravindakrishnan@gmail.com>
2025-07-07 14:51:01 +0530

    Email Automation with Agent

---
+++ src/automate_email.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor, create_structured_chat_agent
+from langchain_core.tools import Tool
+from langchain_groq import ChatGroq
+from email_setup import send_email
+
+load_dotenv()
+
+# tools = [
+#     Tool(
+#     name="Email",
+#     func=send_email,
+#     description=(
+#         "Use this to send an email. "
+#         "Input should include: recipient email, subject, and body of the email. "
+#         "Format: recipient_name, subject, body"
+#         "The function contains three parameters 'recipient_name', 'subject', 'body'"
+#     )
+# )
+# ]
+
+tools = [send_email]
+
+prompt = hub.pull("hwchase17/structured-chat-agent")
+
+
+llm = ChatGroq(
+    api_key= os.getenv("GROQ_API_KEY"),
+    model= "deepseek-r1-distill-llama-70b",
+    temperature= 0
+)
+
+agent = create_structured_chat_agent(
+    llm = llm,
+    tools = tools,
+    prompt= prompt,
+)
+
+agent_executor = AgentExecutor(
+    agent= agent, 
+    tools= tools,
+    verbose = True,
+    handle_parsing_errors= True
+)
+
+response = agent_executor.invoke({    
+    "input": "Send an email to arav302005@gmail.com with subject 'Hello' and body 'Hi Arav, hope you're well.'"
+})
--- src/email_setup.py
+++ src/email_setup.py
@@ -4,6 +4,7 @@ import traceback
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
+from langchain_core.tools import tool
 
 load_dotenv()
 
@@ -12,7 +13,18 @@ email_pwd = os.getenv("EMAIL_APP_PASSWORD")
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 
-def send(recipient_name, subject, body):
+@tool
+def send_email(recipient_name: str, subject: str, body: str) -> str:
+    """
+    This is Python function for Sending Email.
+
+    Args:
+        recipient_name: The Email name of the Receiver
+        subject: The subject of the Email
+        body: The body of the Email
+    
+    Returns: None
+    """
     try:
         msg = MIMEMultipart()
         msg['From'] = email_client
@@ -25,10 +37,10 @@ def send(recipient_name, subject, body):
             server.starttls()
             server.login(email_client, email_pwd)
             server.sendmail(email_client, recipient_name, msg.as_string())
-        print("Success")                
+        return "Success"                
 
     except Exception as e:
-        traceback.print_exc()
+        return f"Error in Sending the Email: {e}"
 
 # Manual Testing
-# send('arav302005@gmail.com', 'Hey', "Testing successful")
+# send_email('siddharth.sulur@gmail.com', 'Hey', "Testing successful")
